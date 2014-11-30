a87d1731cfea373326dc9c5e3f123a04f4ca2f32
Liam Daly <liamw950@gmail.com>
2014-11-30 16:57:17 +1030

    Remove redundant debug flag and add input flag to simplify code

--- protein.py
+++ protein.py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python3
 
 #######TODO
-#Respect verbose with error messages and Remove debug in favour of it
 #Refactor?
 
 import argparse
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("output", help="this is the format the output should be: DNA, mRNA or AA")
+parser.add_argument("output type", help="this is the format the output should be: DNA, mRNA or AA")
+parser.add_argument("input type", help="this is the format the input should be: DNA, mRNA or AA")
 parser.add_argument("sequence",
                     help="this is a sequence of DNA, mRNA or AA and it's type can be infered")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
-parser.add_argument("-d", "--debug", action="store_true",
-                    help="show debugging messages")
 
 args = parser.parse_args()
-output = args.output.lower()
+output = args.output_type.lower()
+input_type = args.input_type.lower()
 sequence = list(args.sequence.lower())
 
-### Ensure correct output is being used
-if args.debug:
-    print("Here's the output type:", output)
-if output != "aa" and output != "mrna" and output != "dna":
-    #verbose only: print("Not DNA, mRNA or AA. Please change your output")
+### Ensure correct output and input types are being used
+if args.verbose:
+    print("Here's the output type:", output_type)
+    print("Here's the input type:", output_type)
+if output_type != "aa" or output_type != "mrna" or output_type != "dna":
+    #verbose only: print("Not DNA, mRNA or AA. Please change your output type")
+    sys.exit(1)
+if input_type != "aa" or input_type != "mrna" or input_type != "dna":
+    #verbose only: print("Not DNA, mRNA or AA. Please change your input type")
     sys.exit(1)
 
 ### Remove punctuation
-if args.debug:
+if args.verbose:
     print("Here's the sequence before removing punctuation:", sequence)
 for x in sequence:
     if x == ",":
         sequence.remove(",")
     elif x == "-":
         sequence.remove("-")
-if args.debug:
+if args.verbose:
     print("Here's the sequence after removing punctuation:", sequence)
-
-### Find what the input sequence type is
-# How fast is having a "not" in there?
-# Create isType functions to make the testing of input sequences more rigerious and correct
-    if "m" in sequence:
-        input = "aa"
-    elif "u" in sequence and "t" not in sequence:
-        input = "mrna"
-    elif "t" in sequence and "u" not in sequence:
-        input = "dna"
-    else:
-        sys.exit(1)
-if args.debug:
-    print("Input:", input)
