b86aee9cc11580c015835a261e3c9c0d72269abf
Liam Wigney <liamw950@gmail.com>
2014-12-07 18:07:37 +1030

    Finally a working conversion from DNA to mRna. The issue that was faced is that not using enumerate we don't have an index so doing base conversions did nothing. This means this is the first semi-functioning verison

--- protein.py
+++ protein.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
-
 import argparse
 import sys
+
+def dnaToRna(sequencetochange):
+    for index, base in enumerate(sequencetochange):
+        if base == "a":
+            sequencetochange[index] = "u"
+        elif base == "t":
+            sequencetochange[index] = "a"
+        elif base == "g":
+            sequencetochange[index] = "c"
+        elif base == "c":
+            sequencetochange[index] = "g"
+        elif base == "-" or base == ",":
+            sequencetochange[index] = base
+        else:
+            # verbose message
+            sys.exit(1)
+    return sequencetochange
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_type", help="this is the format the input should be: DNA, mRNA or AA")
@@ -16,9 +33,6 @@ args = parser.parse_args()
 output_type = args.output_type.lower()
 input_type = args.input_type.lower()
 sequence = list(args.sequence.lower())
-
-
-
 
 # ensure correct output and input types are being used
 # see codecomplete 2 for a better way of doing this using arrays
@@ -37,22 +51,10 @@ if output_type == input_type:
     #verbose message
     sys.exit(1)
 
-
 # do the transformation
 if input_type == "dna" and output_type == "mrna":
-    for base in sequence:
-        if base == "a":
-            base = "u"
-        elif base == "t":
-            base = "a"
-        elif base == "g":
-            base = "c"
-        elif base == "c":
-            base = "g"
-        elif base == "-" or base == ",":
-            base = base
-        else:
-            # verbose message
-            sys.exit(1)
-    out_sequence = ''.join(sequence).upper()
-    print(out_sequence)
+    sequence = dnaToRna(sequence)
+
+
+out_sequence = ''.join(sequence).upper()
+print(out_sequence)
