c1296d1e4c6fecf5908b8d7d47447108669f60af
Liam Wigney <liamw950@gmail.com>
2014-12-07 17:34:15 +1030

    Add the transformation for dna to mrna

--- protein.py
+++ protein.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("output_type", help="this is the format the output should be: DNA, mRNA or AA")
 parser.add_argument("input_type", help="this is the format the input should be: DNA, mRNA or AA")
 parser.add_argument("sequence",
-                    help="this is a sequence of DNA, mRNA or AA")
+                    help="this is a sequence of DNA, mRNA or AA whith both - and , allowed as separators")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 
@@ -21,6 +21,7 @@ sequence = list(args.sequence.lower())
 
 
 # ensure correct output and input types are being used
+# see codecomplete 2 for a better way of doing this using arrays
 if args.verbose:
     print("Here's the output type:", output_type)
     print("Here's the input type:", input_type)
@@ -47,3 +48,19 @@ for x in sequence:
 if args.verbose:
     print("Here's the sequence after removing punctuation:", sequence)
 
+# do the transformation
+if input_type == "dna" and output_type == "mrna":
+    for base in sequence:
+        if base == "a":
+            base = "u"
+        elif base == "t":
+            base = "a"
+        elif base == "g":
+            base = "c"
+        elif base == "c":
+            base = "g"
+        else:
+            # verbose message
+            sys.exit(1)
+    out_sequence = ''.join(sequence).upper()
+    print(out_sequence)
