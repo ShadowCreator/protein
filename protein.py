07681b18c585c89acd4b605be4def1e9d65ed826
Liam Wigney <liamw950@gmail.com>
2014-12-12 13:58:13 +1030

    Remove old comments, print out errors and fix small logic error

--- protein.py
+++ protein.py
@@ -38,7 +38,6 @@ def rnaToDna(sequencetochange):
     return sequencetochange
 
 def rnaToAa(sequencetochange):
-    # remove punctuation
     if args.verbose:
         print("Here's the sequence befande removing punctuation:", sequence)
     for x in sequence:
@@ -50,7 +49,7 @@ def rnaToAa(sequencetochange):
         print("Here's the sequence after removing punctuation:", sequence)
     
     # add punctuation after each aa
-return sequencetochange
+    return sequencetochange
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_type", help="this is the format the input should be: DNA, mRNA or AA")
@@ -65,22 +64,20 @@ output_type = args.output_type.lower()
 input_type = args.input_type.lower()
 sequence = list(args.sequence.lower())
 
-# ensure correct output and input types are being used
 # see codecomplete 2 for a better way of doing this using arrays
 if args.verbose:
     print("Here's the output type:", output_type)
     print("Here's the input type:", input_type)
 if output_type != "aa" and output_type != "mrna" and output_type != "dna":
-    #verbose only: print("Not DNA, mRNA and AA. Please change your output type")
+    print("Not DNA, mRNA and AA. Please change your output type")
     sys.exit(1)
 if input_type != "aa" and input_type != "mrna" and input_type != "dna":
-    #verbose only: print("Not DNA, mRNA and AA. Please change your input type")
+    print("Not DNA, mRNA and AA. Please change your input type")
     sys.exit(1)
-if (input_type == "dna" or input_type == "mrna") and output_type == "aa":
+if input_type == "aa" and ( output_type == "dna" or output_type == "mrna"):
     #verbose
     sys.exit(1)
 
-# do the transformation
 if output_type == input_type:
     sequence = sequence
 elif input_type == "dna" and output_type == "mrna":
